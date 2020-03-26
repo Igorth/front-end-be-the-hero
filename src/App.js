421f0b9a1e9f3fc67a07655e56ab21c10787a150
Igorth <igordiasth@gmail.com>
2020-03-26 18:19:39 +1300

    global css, logon component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import React, { useState } from 'react';
-import Header from './Header';
-
+import Logon from './pages/Logon';
+import './global.css';
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  function increment() {
-    setCounter(counter + 1);
-  }
-
   return (
-    <div>
-      <Header>Contador: { counter }</Header>
-      <button onClick={increment}>Adicionar</button>
-    </div>    
+    <Logon />   
   );
 }
 
