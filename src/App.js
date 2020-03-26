eaa99474bb3e08a8abb33ba74f151de4a5841963
Igorth <igordiasth@gmail.com>
2020-03-26 19:08:43 +1300

    routes and page logon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
-import Logon from './pages/Logon';
+
 import './global.css';
+
+import Routes from './routes';
+
 function App() {
   return (
-    <Logon />   
+    <Routes />   
   );
 }
 
